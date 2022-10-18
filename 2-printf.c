40bd142097dea80633c70c892454da6745c8e0ff
Cyberod <110993874+Cyberod@users.noreply.github.com>
2022-10-18 17:44:42 +0100

    Update 2-printf.c

--- 2-printf.c
+++ 2-printf.c
@@ -90,6 +90,7 @@ int print_octal(va_list ap, params_t *params)
 	unsigned long l;
 	char *str;
 	int c = 0;
+
 	if (params->l_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
